dcc9a223000f893f215403212d554416d3a50721
jaanus <zaar2213@gmail.com>
2025-01-18 12:34:24 +0200

    added basic login functionality

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -1,40 +1,56 @@
 import { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import styles from './Menu.module.css';
 
+let LOGGEDIN = true;
+
 const Menu = () => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    // check login
+    if (LOGGEDIN) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [location]);
+
+  const logOut = () => {
+    LOGGEDIN = false;
+  };
   return (
     <div className={styles.menu}>
       <span className={styles.buttons}>
-        <NavLink className={styles.button_link} to='/'>Products</NavLink>
+        <NavLink className={styles.button_link} to='/'>
+          Products
+        </NavLink>
       </span>
       {loggedIn && (
         <span className={styles.buttons}>
-          <NavLink className={styles.button_link} to='/profile'>Profile</NavLink>
+          <NavLink className={styles.button_link} to='/profile'>
+            Profile
+          </NavLink>
         </span>
       )}
       {!loggedIn && (
         <span className={styles.buttons}>
-          <NavLink className={styles.button_link} to='/login'>Login</NavLink>
+          <NavLink className={styles.button_link} to='/login'>
+            Login
+          </NavLink>
         </span>
       )}
       {loggedIn && (
         <span className={styles.buttons}>
-          <NavLink className={styles.button_link} to='/'>Logout</NavLink>
+          <NavLink className={styles.button_link} onClick={logOut} to='/'>
+            Logout
+          </NavLink>
         </span>
       )}
     </div>
   );
-  // return (
-  //   <div className={styles.menu}>
-  //     <span className={styles.buttons}>Products</span>
-  //     {loggedIn && <span className={styles.buttons}>Profile</span>}
-  //     {!loggedIn && <span className={styles.buttons}>Login</span>}
-  //     {loggedIn && <span className={styles.buttons}>Logout</span>}
-  //   </div>
-  // );
 };
 
 export default Menu;
